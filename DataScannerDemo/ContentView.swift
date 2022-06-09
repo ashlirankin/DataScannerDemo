98c3c1df7263bb15eccee2f1e7f0d9f4e266f249
ashlirankin18@gmail.com <42153710+Ashlirankin18@users.noreply.github.com>
2022-06-09 17:54:10 -0400

    Initial Commit

---
+++ DataScannerDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  DataScannerDemo
+//
+//  Created by Ashli Rankin on 6/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DataScannerDemo/DataScannerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataScannerDemoApp.swift
+//  DataScannerDemo
+//
+//  Created by Ashli Rankin on 6/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DataScannerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
